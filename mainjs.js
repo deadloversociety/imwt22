c8cdcf00f23d088871e9f6e56e8b526e0061dae0
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-05-25 19:09:08 +0200

    pagina medea ma si può fare di meglio

---
+++ mainjs.js
@@ -0,0 +1,38 @@
+function showEur() {
+    var x = document.getElementById("MedeaEur");
+    var y = document.getElementById("MedeaApp");
+    var z = document.getElementById("MedeaOvi");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+function showApR() {
+    var x = document.getElementById("MedeaApp");
+    var y = document.getElementById("MedeaEur");
+    var z = document.getElementById("MedeaOvi");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+  function showOvd() {
+    var x = document.getElementById("MedeaOvi");
+    var y = document.getElementById("MedeaEur");
+    var z = document.getElementById("MedeaApp");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  } 
