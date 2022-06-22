1174e9be89e24d7481af2fd46b036ac2ea9836c1
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-06-22 23:37:28 +0200

    storage fallimentare (original in commetn)

--- scripts/switchstyle.js
+++ scripts/switchstyle.js
@@ -1,15 +1,16 @@
-//https://stackoverflow.com/questions/41355371/dynamically-switching-css-files
+/*Invalid sources:
+    - https://stackoverflow.com/questions/41355371/dynamically-switching-css-files
+    - https://stackoverflow.com/questions/67105891/switch-between-2-stylesheets-and-perpetuate-user-preference-with-local-storage 
+    - https://stackoverflow.com/questions/49827781/how-would-i-get-a-button-to-change-stylesheets-be-saved-to-local-storage-and-a */
 
-var cssStyle = document.getElementById('currentlocalcss');
-
-window.onload = function () {
-    if (localStorage && localStorage.getItem("style"))
-        cssStyle.href = localStorage.getItem("style");
-};
-
-function setStyle(newStyle) {
-    cssStyle.href = newStyle;
-
-    if (localStorage)
-        localStorage.setItem("style", newStyle);
-};
+let darkEnabled = Boolean(localStorage.darkEnabled);
+const cssElem = document.getElementById('currentlocalss');
+if (!darkEnabled) cssElem.href = 'css/todaycss.css';
+else cssElem.href = 'css/todaycss.css';
+document.getElementById("bauhaus")
+  .addEventListener('click', function () {
+    darkEnabled = !darkEnabled;
+    localStorage.darkEnabled = darkEnabled;
+    if (!darkEnabled) cssElem.href = 'css/todaycss.css';
+    else cssElem.href = 'css/todaycss.css';
+  });
