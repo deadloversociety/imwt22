0c684f586534d415bb28d77fd37b2713f2112ce0
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-06-25 23:51:58 +0200

    nuova proposta switchstyle: still :(

--- scripts/switchstyle.js
+++ scripts/switchstyle.js
@@ -3,14 +3,18 @@
     - https://stackoverflow.com/questions/67105891/switch-between-2-stylesheets-and-perpetuate-user-preference-with-local-storage 
     - https://stackoverflow.com/questions/49827781/how-would-i-get-a-button-to-change-stylesheets-be-saved-to-local-storage-and-a */
 
-let darkEnabled = Boolean(localStorage.darkEnabled);
-const cssElem = document.getElementById('currentlocalss');
-if (!darkEnabled) cssElem.href = 'css/todaycss.css';
-else cssElem.href = 'css/todaycss.css';
-document.getElementById("bauhaus")
-  .addEventListener('click', function () {
-    darkEnabled = !darkEnabled;
-    localStorage.darkEnabled = darkEnabled;
-    if (!darkEnabled) cssElem.href = 'css/todaycss.css';
-    else cssElem.href = 'css/todaycss.css';
-  });
+var cssElem = document.getElementById('currentlocalss');
+
+window.onload = function () {
+  currentCss = sessionStorage.getItem('displayedCSS');
+  cssElem.setAttribute('href', currentCss);
+}
+
+function switchTheme(styleSheet) {
+  sessionStorage.setItem('displayedCSS', styleSheet);
+  cssElem.setAttribute('href', styleSheet);
+  
+  // medea.html x futurecss.css Avoiding music to continue when changing style
+  var myAudio = document.getElementById("myAudio");
+  myAudio.pause();
+}
